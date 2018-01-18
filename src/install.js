ad11e1788a0b436c9b313ee2cf6f0721b112bd33
Leo Bergman <leo@pandacreative.se>
2018-01-18 19:47:57 +0100

    chore: Remove test logs

--- src/install.js
+++ src/install.js
@@ -5,20 +5,15 @@ const pjson = require(process.cwd() + '/package.json');
 const npmModuleName = pjson.name;
 const haxeModuleName = npmModuleName.substr(npmModuleName.lastIndexOf('/') + 1);
 installHaxeModule(pjson.version);
-console.log('npmModuleName', npmModuleName);
 function installHaxeModule(version) {
     exec('cd .. && npm prefix', (prefixErr, prefixStdOut, prefixStdErr) => {
         if (!version) {
             console.log('Failed to install hacksaw-core: Couldn\'t read version from package.');
             return;
         }
-        console.log('prefixStdOut:', prefixStdOut);
-
         const commaVersion = version.split('.').join(',').replace('\n', '');
         const parentDir = prefixStdOut.replace('\n', '') + '/';
         const npmDir = parentDir + 'node_modules/' + npmModuleName;
-        console.log('parentDir:', parentDir);
-        console.log('npmDir:', npmDir);
         if (!fs.pathExistsSync(npmDir)) {
             console.log('No parent module. Skipping haxelib install.');
             return;
